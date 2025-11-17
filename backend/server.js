7d0516541fcb1dba592a5dfadc3283e3dba038de
pasmarques <pedroaffonsosilvamarques@gmail.com>
2025-11-17 19:05:07 -0300

    feat: post, delete and  getByUserID routes

--- backend/server.js
+++ backend/server.js
@@ -15,17 +15,58 @@ app.get("/", (req, res) => {
 
 app.post("/api/transactions", async (req, res) => {
     try {
-        const { user_id, title, amount, category } = req.body   
-        if (!user_id || !title || !amount || !category === undefined) {  
-            return res.status(400).send("All fields are required") 
+        const { user_id, title, amount, category } = req.body
+        if (!user_id || !title || amount == null || category == null) {
+            return res.status(400).send("All fields are required")
         }
-        const transaction = await sql`INSERT INTO transactions (user_id, title, amount, category) VALUES (${user_id}, ${title}, ${amount}, ${category}) RETURNING *`
-        return res.status(201).json(transaction)
+        const result = await sql`
+            INSERT INTO transactions (user_id, title, amount, category)
+            VALUES (${user_id}, ${title}, ${amount}, ${category})
+            RETURNING *
+        `
+        return res.status(201).json(result[0])
     } catch (error) {
         console.error("Error creating transaction", error)
-        return res.status(500).send("Internal Server Error")
+        return res.status(500).json({ message: "Internal Server Error" })
     }
-} )   
+})
+ 
+
+app.get("/api/transactions/:userId", async (req, res) => {
+    try {
+        const { userId } = req.params
+        const transactions = await sql`SELECT * FROM transactions WHERE user_id = ${userId} ORDER BY created_at DESC`
+        return res.status(200).json(transactions)
+    } catch (error) {
+        console.error("Error fetching transactions", error)
+        return res.status(500).json({ message: "Internal Server Error" })
+    }           
+})
+
+app.delete("/api/transactions/:id", async (req, res) => {
+    try {
+        const { id } = req.params
+        if(isNaN(parseInt(id))) {
+            return res.status(400).json({ message: "Invalid transaction ID" })
+        }
+        const result = await sql`
+            DELETE FROM transactions
+            WHERE id = ${id}
+            RETURNING *
+        `
+       
+        if (result.length === 0) {
+            return res.status(404).json({ message: "Transaction not found" })
+        }
+        return res.status(200).json({
+            message: "Transaction deleted successfully",
+            deleted: result[0]
+        })  
+    } catch (error) {
+        console.error("Error deleting transaction", error)
+        return res.status(500).json({ message: "Internal Server Error" })
+    }
+})
 
 initDB().then(() => {
     app.listen(PORT, () => {
