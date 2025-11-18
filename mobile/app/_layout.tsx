deb8c20e1a70401a9df1ebdc816ef7cca74183d6
pasmarques <pedroaffonsosilvamarques@gmail.com>
2025-11-18 11:34:39 -0300

    feat(auth): integrate Clerk auth & SignOutButton

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,13 +1,16 @@
+import { Slot } from "expo-router";
 import SafeScreen from "@/components/SafeScreen";
-import { HeaderShownContext } from "@react-navigation/elements";
-import { Stack } from "expo-router";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
-    <SafeScreen>
-      <Stack screenOptions={{headerShown: false}}/>
+    <ClerkProvider tokenCache={tokenCache}>
+      <SafeScreen>
+        <Slot />
+      </SafeScreen>
       <StatusBar style="dark" />
-    </SafeScreen>
-  )
+    </ClerkProvider>
+  );
 }
