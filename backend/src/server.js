aa7748a49569c1c75eab964baa4aa8fe51011b8f
pasmarques <pedroaffonsosilvamarques@gmail.com>
2025-11-17 21:27:43 -0300

    refactor(backend): move server.js into src directory

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,8 @@
 import express from "express"
 import dotenv from "dotenv"
-import { initDB } from "./src/config/db.js"
-import rateLimiter from "./src/middleware/rateLimiter.js"
-import transactionsRoute from "./src/routes/transactionsRoutes.js"
+import { initDB } from "./config/db.js"
+import rateLimiter from "./middleware/rateLimiter.js"
+import transactionsRoute from "./routes/transactionsRoutes.js"
 
 dotenv.config()
 
