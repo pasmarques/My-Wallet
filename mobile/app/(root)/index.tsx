f4ac3b285b29639eb0848f56f36b3e1d88a53054
pasmarques <pedroaffonsosilvamarques@gmail.com>
2025-11-18 23:27:26 -0300

    feat: create transaction screen added

--- mobile/app/(root)/index.tsx
+++ mobile/app/(root)/index.tsx
@@ -58,7 +58,7 @@ export default function Page() {
           </View>
   
           <View style={styles.headerRight}>
-            <TouchableOpacity style={styles.addButton} onPress={() => router.push("/create")}>
+            <TouchableOpacity style={styles.addButton} onPress={() => router.push("./create")}>
               <Ionicons name="add" size={20} color="#FFF" />
               <Text style={styles.addButtonText}>Add</Text>
             </TouchableOpacity>
